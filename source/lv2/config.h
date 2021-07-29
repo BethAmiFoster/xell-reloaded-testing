9ddb668be125393733773dca4c8c1444c3e3d86d
BethAmiFoster <beth@soundsgoodtech.com>
2021-07-29 15:50:57 +1000

    added v0.991 Source Tree from Free60/XeLL-Reloaded

---
+++ source/lv2/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_H
+#define	CONFIG_H
+
+#define ELF_MAXSIZE (32*1024*1024)
+
+/**
+*	Configuration
+**/
+#define DEFAULT_THEME
+//#define NO_PRINT_CONFIG         //commented to display config
+
+
+#endif	/* CONFIG_H */
+
---
+++ source/lv2/zlib/xell_lib.h
@@ -0,0 +1,5 @@
+/*
+used for zlib support ...
+*/
+int inflate_read(char *source,int len,char **dest,int * destsize, int gzip);
+int inflate_compare_header(char *source,int len,char *header, int header_sz, int gzip);
